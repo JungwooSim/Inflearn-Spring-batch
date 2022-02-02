fce59653ab55ff0446803525776c3e58f88b38ff
JungwooSim <cscd053@gmail.com>
2022-02-02 14:15:08 +0900

    스프링 배치 실행 - Flow / FlowJob - start() / next()

--- section4-spring-batch/src/main/java/com/example/section4springbatch/FlowJobExample/FlowJobConfiguration.java
+++ section4-spring-batch/src/main/java/com/example/section4springbatch/FlowJobExample/FlowJobConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,6 +26,35 @@ public class FlowJobConfiguration {
                 .on("FAILED").to(flowJobBatchJobStep2()) // flowJobBatchJobStep1() 의 상태값이 "FAILED" 일때 flowJobBatchJobStep2()
                 .end()
                 .build();
+    }
+
+    @Bean
+    public Job flowJobBatchJob2() {
+        return jobBuilderFactory.get("flowJobBatchJob2")
+                .start(flowJobBatchJobFlowA())
+                .next(flowJobBatchJobStep3())
+                .next(flowJobBatchJobFlowB())
+                .next(flowJobBatchJobStep6())
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Flow flowJobBatchJobFlowA() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowJobBatchJobFlowA");
+        flowBuilder.start(flowJobBatchJobStep1())
+                .next(flowJobBatchJobStep2())
+                .end();
+        return flowBuilder.build();
+    }
+
+    @Bean
+    public Flow flowJobBatchJobFlowB() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowJobBatchJobFlowB");
+        flowBuilder.start(flowJobBatchJobStep4())
+                .next(flowJobBatchJobStep5())
+                .end();
+        return flowBuilder.build();
     }
 
     @Bean
@@ -49,6 +80,33 @@ public class FlowJobConfiguration {
         return stepBuilderFactory.get("flowJobBatchJobStep3")
                 .tasklet(((stepContribution, chunkContext) -> {
                     System.out.println("flowJobBatchJobStep3 has executed");
+                    return RepeatStatus.FINISHED;
+                })).build();
+    }
+
+    @Bean
+    public Step flowJobBatchJobStep4() {
+        return stepBuilderFactory.get("flowJobBatchJobStep4")
+                .tasklet(((stepContribution, chunkContext) -> {
+                    System.out.println("flowJobBatchJobStep4 has executed");
+                    return RepeatStatus.FINISHED;
+                })).build();
+    }
+
+    @Bean
+    public Step flowJobBatchJobStep5() {
+        return stepBuilderFactory.get("flowJobBatchJobStep5")
+                .tasklet(((stepContribution, chunkContext) -> {
+                    System.out.println("flowJobBatchJobStep5 has executed");
+                    return RepeatStatus.FINISHED;
+                })).build();
+    }
+
+    @Bean
+    public Step flowJobBatchJobStep6() {
+        return stepBuilderFactory.get("flowJobBatchJobStep6")
+                .tasklet(((stepContribution, chunkContext) -> {
+                    System.out.println("flowJobBatchJobStep6 has executed");
                     return RepeatStatus.FINISHED;
                 })).build();
     }
