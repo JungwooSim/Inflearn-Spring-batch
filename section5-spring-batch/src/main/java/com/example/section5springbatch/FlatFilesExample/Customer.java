a10634033053390a38cb004486cc144d59290db9
JungwooSim <cscd053@gmail.com>
2022-02-09 23:45:56 +0900

    스프링 배치 청크 프로세스 활용 - ItemReader / XML StaxEventItemReader - 예제

--- section5-spring-batch/src/main/java/com/example/section5springbatch/FlatFilesExample/Customer.java
+++ section5-spring-batch/src/main/java/com/example/section5springbatch/FlatFilesExample/Customer.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class Customer {
+    private Long id;
     private String name;
     private int age;
     private String year;
---
+++ section5-spring-batch/src/main/java/com/example/section5springbatch/FlatFilesExample/XMLConfiguration.java
@@ -0,0 +1,78 @@
+package com.example.section5springbatch.FlatFilesExample;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.xstream.XStreamMarshaller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Configuration
+public class XMLConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job xmlBatchJob() {
+        return jobBuilderFactory.get("xmlBatchJob")
+                .start(xmlBatchJobBatchJobStep1())
+//                .next(xmlBatchJobsBatchJobStep2())
+                .build();
+    }
+
+    @Bean
+    public Step xmlBatchJobBatchJobStep1() {
+        return stepBuilderFactory.get("xmlBatchJobBatchJobStep1")
+                .<Customer, Customer>chunk(3)
+                .reader(customItemReader1())
+                .writer(customItemWriter())
+                .build();
+    }
+
+    @Bean
+    public ItemReader<? extends Customer> customItemReader1() {
+        return new StaxEventItemReaderBuilder<Customer>()
+                .name("staxXml")
+                .resource(new ClassPathResource("customer.xml"))
+                .addFragmentRootElements("customer")
+                .unmarshaller(itemUnmarshaller())
+                .build();
+
+    }
+
+    /**
+     * 직렬화
+     */
+    @Bean
+    public Unmarshaller itemUnmarshaller() {
+        Map<String, Class<?>> aliases = new HashMap<>();
+        aliases.put("customer", Customer.class);
+        aliases.put("id", Long.class);
+        aliases.put("name", String.class);
+        aliases.put("age", Integer.class);
+        XStreamMarshaller xStreamMarshaller = new XStreamMarshaller();
+        xStreamMarshaller.setAliases(aliases);
+
+        return xStreamMarshaller;
+    }
+
+    @Bean
+    public ItemWriter<Customer> customItemWriter() {
+        return items -> {
+            for (Customer item : items) {
+                System.out.println(item.toString());
+            }
+        };
+    }
+}
