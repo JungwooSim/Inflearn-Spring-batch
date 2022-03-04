2673afda137a40c62d5a6894482e6a28ceb96cf8
JungwooSim <cscd053@gmail.com>
2022-03-04 23:15:32 +0900

    스프링 배치 반복 및 오류 제어 / 목차 소개(프로젝트 준비)

---
+++ section6-spring-batch/src/test/java/com/example/section6springbatch/Section6SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.section6springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section6SpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
