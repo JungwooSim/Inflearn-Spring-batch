2fa9065d5c5cd1c04d9160c08808c53c2cee09d3
JungwooSim <cscd053@gmail.com>
2022-03-13 16:13:07 +0900

    스프링 배치 멀티 스레드 프로세싱 / 기본개념

---
+++ section7-spring-batch/src/main/java/com/example/section7springbatch/Section7SpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.section7springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section7SpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section7SpringBatchApplication.class, args);
+    }
+
+}
---
+++ section7-spring-batch/src/test/java/com/example/section7springbatch/Section7SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.section7springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section7SpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
